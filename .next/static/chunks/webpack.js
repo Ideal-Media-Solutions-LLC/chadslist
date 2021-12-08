661c7ed44eb3399d80f01715cf06117e3c1a64df
Mitch Rosenlund <merosenlund@gmail.com>
2021-12-08 12:07:41 -0800

    Added .next to gitignore and added component folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a40b45ecb12799e"; }
+/******/ 		__webpack_require__.h = function() { return "ab8bbb8be66c7e52"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
