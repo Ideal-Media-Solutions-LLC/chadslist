1143d261a872b7a70250a2b9a72d01c3cec939b2
Mitch Rosenlund <merosenlund@gmail.com>
2021-12-09 08:06:04 -0800

    Sequelize associations set up

--- backend/db/db.js
+++ backend/db/db.js
@@ -16,10 +16,11 @@ const User = sequelize.define('User', {
   email: DataTypes.STRING,
   photoUrl: DataTypes.STRING,
   // Maybe have a user location (Like they most recently signed in from)
-  location: DataTypes.INTEGER
+  location: DataTypes.INTEGER,
+  // I'm not sure how we will want to track charity status? A boolean?
 });
 
-const Item = sequelize.define('User', {
+const Item = sequelize.define('Item', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
@@ -27,6 +28,7 @@ const Item = sequelize.define('User', {
   },
   name: DataTypes.STRING,
   description: DataTypes.STRING,
+  // Right now we just will be able to support a single picture. Make a sepparate image table to support multiple pictures?
   imageUrl: DataTypes.STRING,
   // Have the item location be based off the users current signed in location but they can change it if they want to
   location: DataTypes.INTEGER,
@@ -35,32 +37,35 @@ const Item = sequelize.define('User', {
     defaultValue: false
   },
   // We might also do a separate category table and then have this be a foreign key
-  category: Datatypes.STRING
+  category: DataTypes.STRING,
+  donorId: DataTypes.INTEGER
 })
 
-const Claim = sequelize.define('User', {
+const Claim = sequelize.define('Claim', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  userId: DataTypes.INTEGER,
+  claimerId: DataTypes.INTEGER,
   itemId: DataTypes.INTEGER,
+  status: DataTypes.STRING,
   // I don't think we need to do a date or anything because sequelize will automatically put a created_at date
 })
 
-const Receipt = sequelize.define('User', {
+const Receipt = sequelize.define('Receipt', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  userId: DataTypes.INTEGER,
   // We have a couple routes we can go here. Either have the Claim id, the item id, or both. For now I'm just going to do claim id
-  claimId: DataTypes.INTEGER
+  claimId: DataTypes.INTEGER,
+  condition: DataTypes.STRING,
+  value: DataTypes.INTEGER
 })
 
-const Message = sequelize.define('User', {
+const Message = sequelize.define('Message', {
   // We have a couple different routes we can go with this as well. For now I'm just going to go with having each message be sepparate instead of having a chat log. That way we can have a date associated with each message
   id: {
     type: DataTypes.INTEGER,
@@ -68,20 +73,46 @@ const Message = sequelize.define('User', {
     autoIncrement: true
   },
   // I'm not sure if we want to differentiate between claimant and donor... probably it would be a good idea so I will. I can imagine functionality that it would help with.
-  donorId: DataTypes.INTEGER,
-  claimantId: DataTypes.INTEGER,
-  time: DataTypes.DATE,
+  from: DataTypes.INTEGER,
+  to: DataTypes.INTEGER,
+  // Probably don't actually need to have a time since sequelize automatically adds a created_at
   message: DataTypes.STRING
 })
+
+
+// Setting up associations (Relationships)
+// Each donor (User) can have many donations (Items)
+User.hasMany(Item, {foreignKey: 'donorId'});
+Item.belongsTo(User);
+
+// Each claimer (User) can have many claims
+User.hasMany(Claim, {foreignKey: 'claimantId'});
+Claim.belongsTo(User);
+
+// Each Item can have many claims
+Item.hasMany(Claim, {foreignKey: 'itemId'});
+Claim.belongsTo(Item);
+
+// Each claim can have one receipt (Foreign key is stored on the receipt)
+Claim.hasOne(Receipt, {foreignKey: 'claimId'});
+Receipt.belongsTo(Claim);
+
+// Each message is from one user
+User.hasMany(Message, {foreignKey: 'from'});
+Message.belongsTo(User);
+
+// Each message is to one user
+User.hasMany(Message, {foreignKey: 'to'});
+Message.belongsTo(User);
 
 
 
 const testConnection = async () => {
   try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    await sequelize.sync({force: true});
+    console.log('Syncing complete');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('Unable to sync to the database:', error);
   }
 }
 
