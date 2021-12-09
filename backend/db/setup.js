724f4a75c552e8e344ee42c5e78b385fbc13e8e6
Mitch Rosenlund <merosenlund@gmail.com>
2021-12-09 15:52:24 -0800

    Cleaned up setup.js file

--- backend/db/setup.js
+++ backend/db/setup.js
@@ -9,16 +9,6 @@ const testConnection = async () => {
   try {
     await sequelize.sync();
     console.log('Syncing complete');
-    await User.sync();
-    console.log('User Syncing complete');
-    await Item.sync();
-    console.log('Item Syncing complete');
-    await Message.sync();
-    console.log('Message Syncing complete');
-    await Receipt.sync();
-    console.log('Receipt Syncing complete');
-    await Claim.sync();
-    console.log('Claim Syncing complete');
   } catch (error) {
     console.error('Unable to sync to the database:', error);
   }
