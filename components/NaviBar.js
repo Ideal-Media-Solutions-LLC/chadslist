ca717853b75276369d2de1d0cb06bd5171b79a02
Ximing Chen <steven.xm824@gmail.com>
2021-12-08 17:15:07 -0800

    complete basic setup for Navi Bar

--- components/NaviBar.js
+++ components/NaviBar.js
@@ -1,13 +1,12 @@
 import {useState} from 'react';
 import Link from 'next/link';
-import CloseButton from 'react-bootstrap/CloseButton';
+
 
 const NaviBar = (props) => {
 
 
   return (
     <div >
-      <CloseButton />
       <div> > </div>
       <div>
         <Link href="/">
@@ -23,6 +22,11 @@ const NaviBar = (props) => {
         <Link href="/claim">
          <a>My Claims</a>
          </Link>
+      </div>
+      <div>
+        <Link href="/post">
+         <a>Post an Item</a>
+        </Link>
       </div>
       <div>
         <Link href="/">
