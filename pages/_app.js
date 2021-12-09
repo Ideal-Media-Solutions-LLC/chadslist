48394008664b3a431e55b8c84a3162dd69c8ea17
David Han <davidhan2392@gmail.com>
2021-12-09 15:35:38 -0800

    added navbar to app component so it cna persist per page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import AuthState from '../context/auth/AuthState';
+import Navbar from '../components/NaviBar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthState>
+      <Navbar />
       <Component {...pageProps} />
     </AuthState>
   )
