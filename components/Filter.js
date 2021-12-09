2d2abcce9669fa7ed113981667ba5c2e4285d775
Devon Li <devonli102@gmail.com>
2021-12-09 11:03:40 -0800

    added search bar and item view; temporally placing  item view inside filter view

--- components/Filter.js
+++ components/Filter.js
@@ -1,28 +1,43 @@
-import { Nav, Button, Navbar } from 'react-bootstrap';
+import { Nav, Button, Navbar, Collapse, Fade } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {useState} from 'react';
+import { useState } from 'react';
 
-const Filter = (props) => {
+import ItemView from './ItemView.js'
+
+const Filter = ({handleClick}) => {
+  const [show, setShow] = useState(true);
+
 
   return (
     <>
-      <div>Here is Filter</div>
-      <div>Logo</div>
-      <Button>sign up button</Button>
-      <Button>login button</Button>
-      <Nav defaultActiveKey="/home" className="flex-column">
-        <Navbar.Brand >Select Category</Navbar.Brand>
-        <Nav.Link eventKey="filter-1">Apparel</Nav.Link>
-        <Nav.Link eventKey="filter-2">Electronics</Nav.Link>
-        <Nav.Link eventKey="filter-3">Entertainment</Nav.Link>
-        <Nav.Link eventKey="filter-4">Garden and Outdoor</Nav.Link>
-        <Nav.Link eventKey="filter-5">Hobbies</Nav.Link>
-        <Nav.Link eventKey="filter-6">Home Goods</Nav.Link>
-        <Nav.Link eventKey="filter-7">Musical Instruments</Nav.Link>
-        <Nav.Link eventKey="filter-8">Office Supplies</Nav.Link>
-        <Nav.Link eventKey="filter-9">Pet Supplies</Nav.Link>
-        <Nav.Link eventKey="filter-10">Sporting Goods</Nav.Link>
-      </Nav>
+      <Collapse in={show} >
+        <div id="example-collapse-text">
+          <div>Here is Filter</div>
+          <div>Logo</div>
+          <Button>sign up button</Button>
+          <Button>login button</Button>
+          <Nav defaultActiveKey="/" className="flex-column">
+            <Navbar.Brand >Select Category</Navbar.Brand>
+            <Nav.Link eventKey="filter-1"
+            onClick={() => setShow(!show)}
+            aria-controls="example-collapse-text"
+            aria-expanded={show}>
+            {/* onClick={() => handleClick()}> */}
+              Apparel
+            </Nav.Link>
+            <Nav.Link eventKey="filter-2">Electronics</Nav.Link>
+            <Nav.Link eventKey="filter-3">Entertainment</Nav.Link>
+            <Nav.Link eventKey="filter-4">Garden and Outdoor</Nav.Link>
+            <Nav.Link eventKey="filter-5">Hobbies</Nav.Link>
+            <Nav.Link eventKey="filter-6">Home Goods</Nav.Link>
+            <Nav.Link eventKey="filter-7">Musical Instruments</Nav.Link>
+            <Nav.Link eventKey="filter-8">Office Supplies</Nav.Link>
+            <Nav.Link eventKey="filter-9">Pet Supplies</Nav.Link>
+            <Nav.Link eventKey="filter-10">Sporting Goods</Nav.Link>
+          </Nav>
+          <ItemView />
+        </div>
+      </Collapse>
     </>
   )
 }
