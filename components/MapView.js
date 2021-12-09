9d0e264af13b8e99dccbbd05c34adc50e695f32d
vinhle000 <vinhle000@gmail.com>
2021-12-09 10:53:17 -0800

    Installed Google Maps api dependencies - Currently implementing google maps with react

--- components/MapView.js
+++ components/MapView.js
@@ -1,10 +1,32 @@
 import {useState} from 'react';
 
+// import { GoogleMap } from 'map-loader';
+import {mapsAPI} from '../tempConfig.js'
+import {Loader}  from '@googlemaps/js-api-loader';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+const loader = new Loader(mapsAPI);
+
+loader.load().then( () => {
+  console.log('Maps JS API Loaded')
+})
+
+function displayMap() {
+  const mapOptions = {
+    center ={ lat: -33.860664, lng: 151.208138 },
+    zoom: 14
+  }
+}
+
+
+
+
 const MapView = (props) => {
   const [list, setList] = useState(false);
 
   return (
-    <div> Maps View here</div>
+    <div> Maps View here
+
+    </div>
   )
 }
 
