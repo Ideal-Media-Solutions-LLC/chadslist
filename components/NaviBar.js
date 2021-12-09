2ab2bb1a67317d98065e4d859c104622f821b07e
Ximing Chen <steven.xm824@gmail.com>
2021-12-09 11:46:23 -0800

    complete navi in sidebar

--- components/NaviBar.js
+++ components/NaviBar.js
@@ -1,21 +1,23 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
-import {Nav} from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
 
 const NaviBar = (props) => {
 
 
   return (
-    <Nav defaultActiveKey="/" className="flex-column">
-    <Nav.Link href="/">Home</Nav.Link>
-    <Nav.Link href="/donations">My Donations</Nav.Link>
-    <Nav.Link href="/Claims">My Claims</Nav.Link>
-    <Nav.Link href="/posts">Post an Item</Nav.Link>
-    <Nav.Link eventKey="disabled" disabled>
-      LogOut
-    </Nav.Link>
-  </Nav>
+    <div>
+      <Nav defaultActiveKey="/" className="flex-column">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/donations">My Donations</Nav.Link>
+        <Nav.Link href="/Claims">My Claims</Nav.Link>
+        <Nav.Link href="/posts">Post an Item</Nav.Link>
+        <Nav.Link eventKey="disabled" disabled>
+          LogOut
+        </Nav.Link>
+      </Nav>
+    </div>
   )
 }
 
