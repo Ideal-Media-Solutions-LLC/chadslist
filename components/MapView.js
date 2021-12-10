45be1ed4077134601ad6965104c18a164b1bbb07
vinhle000 <vinhle000@gmail.com>
2021-12-10 11:13:59 -0800

    Implemented google map within component with markers using sample data.

--- components/MapView.js
+++ components/MapView.js
@@ -1,18 +1,39 @@
 import React from 'react'
-import { GoogleMap, LoadScriptNext } from '@react-google-maps/api';
+import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api';
 import {mapsAPI} from '../tempConfig.js'
+import { MdPlace } from 'react-icons/md';
 
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
+//TODO: Remove after real/formatted data is provided
+//SampleTest data for showing markers
+const items = [
+  {
+    name: 'Space Needle',
+    coordinates: { lat: 47.6205, lng: -122.3493 },
+  },
+  {
+    name: "Cal Anderson Park",
+    coordinates: { lat: 47.6173, lng: -122.3195 },
+  },
+]
 
-const center = {
-  lat: 47.6253,
-  lng: -122.3222
-};
 
 function MapView(props) {
+
+  const position = { lat: 47.61483, lng: -122.3146 }
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+
+  const center = {
+    lat: 47.6253,
+    lng: -122.3222
+  };
+
+  const onLoad = marker => {
+    console.log('marker: ', marker)
+
+  }
   return (
     <LoadScriptNext
       googleMapsApiKey={mapsAPI}
@@ -20,13 +41,22 @@ function MapView(props) {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={14}
+        zoom={13}
       >
         { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+      {items.map( (item, index) => {
+        return (
+
+          <Marker
+            onLoad={onLoad}
+            position={item.coordinates}
+          />
+        )
+      })}
+
       </GoogleMap>
     </LoadScriptNext>
   )
 }
 
-export default React.memo(MapView)
+export default MapView
