9d05589b198848a19659ba062357ea68bf88cf4b
vinhle000 <vinhle000@gmail.com>
2021-12-10 08:26:16 -0800

    Implemented Google Maps with @react-google-maps/api provides bindings to the Google Maps JavaScript APIv3.

--- components/MapView.js
+++ components/MapView.js
@@ -1,46 +1,32 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import React from 'react'
+import { GoogleMap, LoadScriptNext } from '@react-google-maps/api';
 import {mapsAPI} from '../tempConfig.js'
 
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const center = {
+  lat: 47.6253,
+  lng: -122.3222
+};
 
-// const map = new google.maps.Map(mapDiv, mapOptions);
-
-const MapView = (props) => {
-  const defaultProps = {
-    center: {
-      lat: 	47.620422,
-      lng: -122.349358
-    },
-    zoom: 11
-  };
-
-
-
+function MapView(props) {
   return (
-
-
-
-      // <div >
-      //   <Image src='/MapViewPlaceHolder.png' width='640' height='780'/>
-      // </div>
-
-      <div style={{ height: '50vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: mapsAPI }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+    <LoadScriptNext
+      googleMapsApiKey={mapsAPI}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={14}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScriptNext>
   )
 }
 
-export default MapView;
+export default React.memo(MapView)
