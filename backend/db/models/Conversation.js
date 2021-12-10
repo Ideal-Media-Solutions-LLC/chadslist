938109c410668b4bb4fd861a20da1689283188ac
David Han <davidhan2392@gmail.com>
2021-12-10 12:12:51 -0800

    working on conversation message table

--- backend/db/models/Conversation.js
+++ backend/db/models/Conversation.js
@@ -3,7 +3,12 @@ const sequelize = require('../db.js');
 const User = require('./User.js');
 
 const Conversation = sequelize.define('conversation', {
-
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true
+  }
 });
+
+Conversation.hasMany(Message, {foreignKey: })
 
 module.exports = Conversation;
