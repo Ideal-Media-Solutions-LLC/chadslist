38c89b37a222de3b62a84ac5f84029e4f4a91475
vinhle000 <vinhle000@gmail.com>
2021-12-09 16:18:53 -0800

    Implemented GoogleMap Api, -Map now displays on screen

--- components/MapView.js
+++ components/MapView.js
@@ -1,38 +1,45 @@
-import {useState} from 'react';
-import Image from 'next/image';
-
-// import { GoogleMap } from 'map-loader';
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
 import {mapsAPI} from '../tempConfig.js'
-import {Loader}  from '@googlemaps/js-api-loader';
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-const loader = new Loader(mapsAPI);
-
-loader.load().then( () => {
-  console.log('Maps JS API Loaded')
-})
-
-function displayMap() {
-  const mapOptions = {
-    center ={ lat: -33.860664, lng: 151.208138 },
-    zoom: 14
-  }
-}
 
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+// const map = new google.maps.Map(mapDiv, mapOptions);
 
 const MapView = (props) => {
-  const [list, setList] = useState(false);
+  const defaultProps = {
+    center: {
+      lat: 	47.620422,
+      lng: -122.349358
+    },
+    zoom: 11
+  };
+
+
 
   return (
-<<<<<<< HEAD
-    <div> Maps View here
 
-=======
-    <div>
-      <Image src='/MapViewPlaceHolder.png' width='640' height='780'/>
->>>>>>> main
+
+
+      // <div >
+      //   <Image src='/MapViewPlaceHolder.png' width='640' height='780'/>
+      // </div>
+
+      <div style={{ height: '50vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: mapsAPI }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
     </div>
+
   )
 }
 
