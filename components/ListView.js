6fa134b08c62fc35504914a56b15473a830d9e6a
Ximing Chen <steven.xm824@gmail.com>
2021-12-10 11:14:16 -0800

    setup list view with dummy data

--- components/ListView.js
+++ components/ListView.js
@@ -1,13 +1,47 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
+import ItemView from './ItemView.js';
+import { Card, Container, Row, Col, Modal } from 'react-bootstrap';
 
 const ListView = (props) => {
   const [map, setMap] = useState(false);
+  const [item, setItem] = useState(false);
+
+  // dummy data to show List
+  const [list, setList] = useState([
+    { name: 'item', img: '/apparel_2.svg' },
+    { name: 'item', img: '/apparel_2.svg' },
+    { name: 'item', img: '/apparel_2.svg' },
+    { name: 'item', img: '/apparel_2.svg' },
+    { name: 'item', img: '/apparel_2.svg' },
+    { name: 'item', img: '/apparel_2.svg' },
+  ]);
+
+  const viewItem = () => setItem(true);
+  const closeItem = () => setItem(false);
+
 
   return (
     <div>
-      <Image src='/ListViewPlaceHolder.png' width='640' height='780'/>
+      <Container>
+        <Row sm={4} md={4}>
+          {list.map((item, index) => <Col md={4}>
+            <Card onClick={viewItem} style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={item.img} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>)}
+        </Row>
+      </Container>
 
+      <Modal show={item} fullscreen={true} onHide={closeItem}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal.Header>
+        {/* <ItemView /> */}
+      </Modal>
     </div>
   )
 }
