59cad59e651206977c82517fcfa2e75aea2e55ce
Jeffrey Stewart <jstewartofficial20111@gmail.com>
2021-12-10 17:43:07 -0600

    updates item status on claim

--- backend/controllers/claim.js
+++ backend/controllers/claim.js
@@ -1,4 +1,5 @@
 const Claim = require('../db/models/Claim.js');
+const Item = require('../db/models/Item.js');
 const claim = async (req, res) => {
   const claimerId = req.body.claimantId;
   const itemId = req.body.itemId;
@@ -6,6 +7,11 @@ const claim = async (req, res) => {
   const UserId = req.body.UserId;
   const claim = await Claim.create({ claimerId, itemId, status, UserId })
     .catch(err => console.log(err));
+  const item = await Item.update({ claimed: true }, {
+    where: {
+      id: itemId
+    }
+  });
   res.status(201).end();
 };
 
