181e690ab3b8e4170cd61a77282ead9ae1aac443
NABAO <xyinaxy@gmail.com>
2021-12-10 23:26:08 -0800

    add fake data generating function for items table

--- backend/db/setup/faker.js
+++ backend/db/setup/faker.js
@@ -18,18 +18,23 @@ const seedUser = async () => {
       users[i].userName = faker.internet.userName();
       users[i].email = faker.internet.email();
       users[i].photoUrl = faker.image.people();
+
       if (i % 10 === 0) {
         users[i].status = 'charity';
       } else {
         users[i].status = 'individual';
       }
-      if (i % 3 === 0) {
+
+      if (i < 40) {
+        // NY
         users[i].latitude = 40.72557420158411;
         users[i].longitude = -74.01148541130824;
-      } else if (i % 3 === 1) {
+      } else if (i >= 40 && i < 70) {
+        // SF
         users[i].latitude = 37.962882809573145;
         users[i].longitude = -122.57822275079111;
       } else {
+        // SA
         users[i].latitude = 47.77658566128346;
         users[i].longitude = -122.41963026610908;
       }
@@ -37,7 +42,7 @@ const seedUser = async () => {
     }
     await User.bulkCreate(users);
   } catch (error) {
-    console.log(error);
+    console.log('error adding data for users', error);
   }
 }
 
@@ -45,8 +50,70 @@ const seedUser = async () => {
 // San Francisco: 37.962882809573145, -122.57822275079111
 // Seattle: 47.77658566128346, -122.41963026610908
 
-const seedItem = () => {
+let categories = [
+  'Apparel',
+  'Electronics',
+  'Entertainment',
+  'Garden and Outdoor',
+  'Hobbies',
+  'Home Goods',
+  'Musical Instruments',
+  'Office Supplies',
+  'Pet Supplies',
+  'Sporting Goods'
+];
 
+let itemStatus = ['unclaimed', 'unclaimed', 'processing', 'claimed'];
+
+let coordinates = {
+  ny: {
+    latitude: 40.72557420158411,
+    longitude: -74.01148541130824
+  },
+  sf: {
+    latitude: 37.962882809573145,
+    longitude: -122.57822275079111
+  },
+  sa: {
+    latitude: 47.77658566128346,
+    longitude: -122.41963026610908
+  }
+};
+
+let items = [];
+const seedItem = async () => {
+  // let items = [];
+  try {
+    for (var i = 0; i < 100; i++) {
+      items[i] = {};
+      items[i].id = i;
+      items[i].name = faker.commerce.productName();
+      items[i].description = faker.commerce.productDescription();
+      items[i].imageUrl = faker.image.fashion();
+      items[i].category = categories[Math.floor(Math.random() * 10)];
+      items[i].status = itemStatus[Math.floor(Math.random() * 4)];
+
+      if (i < 40) {
+        // NY
+        items[i].latitude = (coordinates.ny.latitude - 0.5 + Math.random()).toFixed(12);
+        items[i].longitude = (coordinates.ny.longitude - 0.5 + Math.random()).toFixed(12);
+        items[i].donorId = Math.floor(Math.random() * 40);
+      } else if (i >= 40 & i < 70) {
+        // SF
+        items[i].latitude = (coordinates.sf.latitude - 0.5 + Math.random()).toFixed(12);
+        items[i].longitude = (coordinates.sf.longitude - 0.5 + Math.random()).toFixed(12);
+        items[i].donorId = 40 + Math.floor(Math.random() * 30);
+      } else {
+        // SA
+        items[i].latitude = (coordinates.sa.latitude - 0.5 + Math.random()).toFixed(12);
+        items[i].longitude = (coordinates.sa.longitude - 0.5 + Math.random()).toFixed(12);
+        items[i].donorId = 70 + Math.floor(Math.random() * 30);
+      }
+    }
+    await Item.bulkCreate(items);
+  } catch (error) {
+    console.log('error adding data for items', error);
+  }
 }
 
 // <40 newYork
@@ -113,8 +180,10 @@ const seedClaim = () => {
 const seedAll = async() => {
   await User.sync({force: true});
   await Conversation.sync({force: true})
+  await Item.sync({force: true});
   await seedUser();
   await seedConversation();
+  await seedItem();
 }
 
 seedAll();
