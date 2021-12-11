34e01d20609c2117ba9a31472cd30aa05989bd02
vinhle000 <vinhle000@gmail.com>
2021-12-11 10:56:26 -0800

    Cleanup, and tested infoWindows with images

--- components/MapView.js
+++ components/MapView.js
@@ -7,10 +7,12 @@ import { GoogleMap, LoadScript, Marker, InfoWindow} from '@react-google-maps/api
 const items = [
   {
     name: 'Space Needle',
+    img_url: 'https://cdn.pixabay.com/photo/2016/11/23/01/18/red-panda-1851661_1280.jpg',
     coordinates: { lat: 47.6205, lng: -122.3493 },
   },
   {
     name: "Cal Anderson Park",
+    img_url: 'https://cdn.pixabay.com/photo/2016/11/23/01/15/red-panda-1851650_1280.jpg',
     coordinates: { lat: 47.6173, lng: -122.3195 },
   },
 ]
@@ -38,19 +40,24 @@ function MapView(props) {
     height: '400px'
   };
 
+  const infoWindowStyle = {
+    'object-fit': 'contain',
+    width: '10vh',
+    height: '10vh',
+  }
+
   const center = {
     lat: 47.6253,
     lng: -122.3222
   };
+
 
   const onLoad = marker => {
     console.log('marker: ', marker)
   }
 
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.mapAPI}
-    >
+    <LoadScript googleMapsApiKey={process.env.mapAPI} >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
@@ -61,36 +68,36 @@ function MapView(props) {
         }}
       >
         { /* Child components, such as markers, info windows, etc. */ }
-      {items.map( (item, index) => {
-        return (
-        <Marker
-          key={index}
-          onLoad={onLoad}
-          position={item.coordinates}
-          onClick={(e)=> {
-            setSelected(item)}}
-        />
+        {items.map( (item, index) => {
+          return (
+            <Marker
+              key={index}
+              onLoad={onLoad}
+              position={item.coordinates}
+              onClick={(e)=> {
+                setSelected(item)}}
+            />
+          )}
         )}
-        )})}
 
-          {selected ? (
+        {selected ? (
           <InfoWindow
-            options={{
-              pixelOffset: new window.google.maps.Size(0, -45),
-             }}
+            options={{pixelOffset: new window.google.maps.Size(0, -45) }}
             position={{lat: selected.coordinates.lat, lng: selected.coordinates.lng}}
-
             onCloseClick={() => {
               setSelected(null)
             }}
           >
-          <div>INFO</div>
-          </InfoWindow>
-          ) : null}
 
+            <div style={infoWindowStyle}>
+              <img style={{width: '100%', height: '100'}} src={selected.img_url} />
+            </div>
+
+          </InfoWindow>
+        ) : null}
 
       </GoogleMap>
-    // </LoadScript>
+   </LoadScript>
   )
 }
 
