0d203af327e828fb30c146e7a02557236432cfab
David Han <davidhan2392@gmail.com>
2021-12-13 15:14:11 -0800

    storing location and itemlist in context

--- context/item/ItemReducer.js
+++ context/item/ItemReducer.js
@@ -1,7 +1,16 @@
-// export default(state, action) => {
-//   switch(action.type) {
-//     case
-//     default:
-//       return state
-//   }
-// }
+import {
+  SET_ITEM_LIST
+} from '../types'
+
+export default(state, action) => {
+  switch(action.type) {
+    case SET_ITEM_LIST:
+      return {
+        ...state,
+        itemList: action.payload.data,
+        currentLocation: {lat: action.payload.lat, lng: action.payload.lng}
+      }
+    default:
+      return state
+  }
+}
--- context/item/ItemState.js
+++ context/item/ItemState.js
@@ -1,6 +1,14 @@
 import ItemContext from './ItemContext';
 import ItemReducer from './ItemReducer';
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useContext } from 'react';
+import AuthContext from '../auth/AuthContext';
+import axios from 'axios';
+
+import {
+  SET_ITEM_LIST
+} from '../types';
+
+const API_URL = 'http://localhost:3001/items'
 
 const ItemState = (props) => {
   const initialState = {
@@ -11,11 +19,29 @@ const ItemState = (props) => {
   // useEffect(() => {
   //   getItemsInRadius(coordinates);
   // }, [currentLocation])
-
+  const { user } = useContext(AuthContext);
   const [state, dispatch] = useReducer(ItemReducer, initialState)
 
-  const getItemsInRadius = (lat, lng) => {
-    console.log(lat, lng, 'context')
+  const getItemsInRadius = (lat = 10, lng = 10) => {
+    // FOR LATER: need to have dynamic input for radius for user
+
+    // axios.get(`${API_URL}/?userId=${user.id}&radius=10&latitude=${lat}&longitude=${lng}`)
+
+    axios.get(`${API_URL}/?radius=10&latitude=${lat}&longitude=${lng}`)
+    .then((result) => {
+      const newObj = {
+        data: result.data,
+        lat: lat,
+        lng: lng
+      }
+      dispatch({
+        type: SET_ITEM_LIST,
+        payload: newObj
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
 
   return (
