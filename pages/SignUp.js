ca81cbfa1f2d5c98fb1a907556491162cb97af3a
Anthony Psyk <ajpsyk@gmail.com>
2021-12-13 08:56:11 -0800

    Added profile photo functionality

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -9,8 +9,8 @@ const SignUp = (props) => {
     email: '',
     password: '',
     password2: '',
-    status: '',
-    photoUrl: ''
+    accType: 'Individual',
+    photoUrl: '/profile_placeholder_2.svg'
   })
 
   const { registerUser } = useContext(AuthContext);
@@ -20,58 +20,63 @@ const SignUp = (props) => {
 
     if (form.password !== form.password2) {
       alert('Passwords are not matching. Try again')
+    } else {
+      registerUser(form)
     }
-
-    registerUser(form)
   }
 
   const handleChange = ({ target: { name, value } }) => {
     setForm({ ...form, [name]: value})
   }
 
+  const handleFileChange = (e) => {
+    console.log(URL.createObjectURL(e.target.files[0]));
+    setForm({...form, photoUrl: URL.createObjectURL(e.target.files[0])});
+  }
+
   return (
     <>
-      <Container className="signup-container">
+      <Container className="form-container">
         <Col>
           <Form className="signup-form" onSubmit={handleSubmit}>
-          <Image className="login-logo" src="/Chads_List_2.svg" alt="chadslist_logo" width={300}/>
             <Form.Group controlId="formFile" className="mb-3">
-              <Form.Label>Profile Photo</Form.Label>
-              <Form.Control placeholder="Upload profile picture" type="file" name="photoUrl" value={form.photoUrl}/>
+              <Image className="login-logo" src="/Chads_List_2.svg" alt="chadslist_logo" width={425}/>
+              <Image className="default-profile-pic" src={form.photoUrl} width={300} roundedCircle/>
+              <Form.Label>Upload Photo</Form.Label>
+              <Form.Control placeholder="Upload profile picture" type="file" name="photoUrl" onChange={handleFileChange}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
+              {/* <Form.Label>Email address</Form.Label> */}
               <Form.Control type="email" placeholder="Enter email" name="email" value={form.email} onChange={handleChange} required/>
-              <Form.Text className="text-muted"> We'll never share your email with anyone else. </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Username</Form.Label>
+              {/* <Form.Label>Username</Form.Label> */}
               <Form.Control  placeholder="Your username" name="userName" value={form.userName} onChange={handleChange} required/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Password</Form.Label>
+              {/* <Form.Label>Password</Form.Label> */}
               <Form.Control type="password" placeholder="Password" name="password" value={form.password} onChange={handleChange} required/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Repeat Password</Form.Label>
+            <Form.Group className="mb-3">
+              {/* <Form.Label>Repeat Password</Form.Label> */}
               <Form.Control type="password" placeholder="Re-enter password" name="password2" value={form.password2} onChange={handleChange} required/>
             </Form.Group>
             <Form.Group>
-              <Form.Label>Are you a charity or an individual?</Form.Label>
-              <Form.Select className="me-sm-2" id="inlineFormCustomSelect" name="status" value={form.status} onChange={handleChange} required>
-                <option selected="selected" value="individual">Individual</option>
+              <Form.Label htmlFor="accountType">Are you a charity or an individual?</Form.Label>
+              <Form.Select className="me-sm-2" id="inlineFormCustomSelect" name="accountType" value={form.accType} onChange={handleChange} required>
+                <option value="individual">Individual</option>
                 <option value="charity">Charity</option>
               </Form.Select>
             </Form.Group>
             <br></br>
             <Form.Group>
-              <Button className="signup-button" variant="primary" type="submit">
+              <Button id="signup-button-signup-form" variant="primary" type="submit">
                 Sign Up
               </Button>
             </Form.Group>
             <Form.Group className="login-signup-redirect">
               <Form.Label>Already have an account?</Form.Label>
-              <Button className="login-button" variant="primary" href='/Login'>
+              <Button id="login-button-redirect" variant="primary" href='/Login'>
                 Login
               </Button>
             </Form.Group>
