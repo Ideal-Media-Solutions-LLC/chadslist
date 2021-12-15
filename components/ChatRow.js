b72a44ef235a5292ff7a940d64a95e00d61b9cb2
David Han <davidhan2392@gmail.com>
2021-12-15 15:17:37 -0800

    fixed issue where data was not being accessed quick enough when destructured

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import ChatContext from '../context/chat/ChatContext';
 import { InputGroup, Button, FormControl, Form, Popover, Container, Row, Col, Modal, Image } from 'react-bootstrap';
 import MessageView from './MessageView.js';
@@ -13,7 +13,8 @@ const ChatRow = ({ message, userId }) => {
   const [show, setShow] = useState(false);
   const receiverId = smallerId == userId ? largerId : smallerId
 
-  const { user : { userName, email, photoUrl } } = message;
+  const { user } = message;
+  // const { user : { userName, email, photoUrl } } = message;
   const data = {
     socket: socket,
     sender: userId,
@@ -21,10 +22,15 @@ const ChatRow = ({ message, userId }) => {
     id: id
   }
 
+  console.log(user)
+
   const joinRoom = () => {
     socket.emit("join_chat", id)
   }
 
+  if(!user) {
+    return null
+  } else {
   return (
     <>
   <Row className="chat-row-container" onClick={() => {
@@ -32,10 +38,10 @@ const ChatRow = ({ message, userId }) => {
     getMessages(userId, receiverId)
     setShow(!show)}}>
     <div className="chat-row">
-      <Avatar style={{ height: '50px', width: '50px' }} src={photoUrl} alt="D" />
+      <Avatar style={{ height: '50px', width: '50px' }} src={user.photoUrl} alt={user.userName.slice(0, 1).toUpperCase()} />
       {/* <Image src={photoUrl} roundedCircle className="chat-icon" /> */}
       <div className="chat-row-text">
-        <p className="chat-username">{userName}</p>
+        <p className="chat-username">{user.userName}</p>
         <p>Item Name</p>
       </div>
     </div>
@@ -43,10 +49,11 @@ const ChatRow = ({ message, userId }) => {
 
   <Modal centered show={show} fullscreen={true} onHide={() => setShow(!show)} >
   <Modal.Header closeButton>Chat</Modal.Header>
-  <MessageView photoUrl={photoUrl} sender={userId} receiver={receiverId} id={id} socket={socket}/>
+  <MessageView photoUrl={user.photoUrl} sender={userId} receiver={receiverId} id={id} socket={socket}/>
   </Modal>
 </>
   )
+  }
 }
 
 export default ChatRow
