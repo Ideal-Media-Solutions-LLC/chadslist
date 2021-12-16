9004101dc91a5ec67fd9313a78ba8f7f91630157
David Han <davidhan2392@gmail.com>
2021-12-16 14:34:16 -0800

    updated navbar for message page

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -5,7 +5,7 @@ import NaviBar from '../../components/NaviBar.js';
 import MessageView from '../../components/MessageView';
 import ChatRow from '../../components/ChatRow';
 import AuthContext from '../../context/auth/AuthContext';
-import Loader from '../../components/Loader';
+import Avatar from '@mui/material/Avatar';
 
 const ChatPage = () => {
   const { user } = useContext(AuthContext)
@@ -13,7 +13,7 @@ const ChatPage = () => {
 
   useEffect(() => {
     getAllMessages();
-  }, [messagePageList])
+  }, [])
 
 
   const [showNavi, setNavi] = useState(false);
@@ -21,16 +21,22 @@ const ChatPage = () => {
   const closeNavi = () => setNavi(false);
 
   if (loading) {
-    return <Loader />
+    return <p>...Loading</p>
   } else {
     return (
       <>
       <Row className="header">
             <Col>
-              <img className="home-page-logo" src='/Chads_list_2.svg' width='300' height='100' />
+              <img className="home-page-logo" src='/Chads_list_2.svg' width='230' height='100' />
             </Col>
             <Col className="home-page-buttons">
-              <img id="hamburger-menu-home-page" onClick={naviShow} src='/dropdown_menu.svg' width='50' height='50' />
+              {user ? <div className="avatar-header-row" onClick={naviShow}>
+                <Avatar alt="Travis Howard" src={user.photoUrl} style={{ height: '35px', width: '35px', marginRight: '8px' }}/>
+                <div style={{ marginTop: '12px', fontWeight: 'bold', display: 'flex', flexWrap: 'wrap' }}>
+                <p>{user.userName}</p>
+                </div>
+              </div> :   <img id="hamburger-menu-home-page" onClick={naviShow} src='/dropdown_menu.svg' width='50' height='50' /> }
+
               <Offcanvas placement='end' show={showNavi} onHide={closeNavi} >
                 <Offcanvas.Header closeButton></Offcanvas.Header>
                 <NaviBar close={closeNavi}/>
