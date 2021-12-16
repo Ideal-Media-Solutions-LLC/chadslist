739bbbdfd7c399e1c627d41fe4e6b2a1816cc195
vinhle000 <vinhle000@gmail.com>
2021-12-16 14:21:05 -0800

    Refactorin MapView.js, -Added useEffect to reset the selectedMarker item to be null when new viewableItems are provided. This allows the infoWindow to be dismissed and not rendered.
    ItemView.js now converts the Item longitude and latitude to a human readable address string and displays that within the view.

--- components/ItemView.js
+++ components/ItemView.js
@@ -1,7 +1,7 @@
 import { Button, Card, CloseButton, Modal } from 'react-bootstrap';
 import MessageView from './MessageView.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import ChatContext from '../context/chat/ChatContext';
 import AuthContext from '../context/auth/AuthContext';
@@ -19,6 +19,7 @@ const ItemView = ({ data, currentPage, revoke }) => {
   const [histClaim, setHistClaim] = useState(true);
   const [histList, setHistList] = useState(true);
   const [page, setPage] = useState(currentPage);
+  const [address, setAddress] = useState('')
 
   const handleHistClaim = (e) => {
     axios.put(`http://localhost:3001/history/claims?itemId=${id}`)
@@ -61,6 +62,27 @@ const ItemView = ({ data, currentPage, revoke }) => {
       socket.emit("join_chat", conversationId)
     }
 
+
+  /*
+  Converts location coordinates to Human Readable address string
+  Results may include a Google 'Plus Code' due to the mock data point
+  locations of items being in places that have no proper address.
+  */
+   useEffect( () => {
+    const geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({location: {
+      lat: data.latitude,
+      lng: data.longitude
+    }})
+    .then( response => {
+      console.log(response)
+      if (response.results[1]) {
+       setAddress(response.results[1].formatted_address)
+      }
+    })}, [data])
+
+
   return (
     <>
       <Card style={{ width: '24.9rem' }}>
@@ -68,7 +90,7 @@ const ItemView = ({ data, currentPage, revoke }) => {
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>Value</Card.Text>
-          <Card.Text>Location</Card.Text>
+          <Card.Text>{address}</Card.Text>
 
           {
             page !== 'history' &&
