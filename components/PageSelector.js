376a7d29a63625be95ea8bd0aec0f612265d3127
Ximing Chen <steven.xm824@gmail.com>
2021-12-17 12:36:01 -0800

    mvp for pagination

--- components/PageSelector.js
+++ components/PageSelector.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import {useState} from 'react';
 import { Pagination } from 'react-bootstrap';
 
 const PageSelector = ({ itemsPerPage, itemsTotal, changePage }) => {
   const pageNumbers = [];
+  const [pageIndex, setPageIndex] = useState(0);
 
   for (let i = 1; i <= Math.ceil(itemsTotal / itemsPerPage); i++) {
 
-    if (i < 4 || (i = Math.ceil(itemsTotal / itemsPerPage))) {
-      pageNumbers.push(<Pagination.Item onClick={() => changePage(i)} key={i}>
+    if (i < 4 || (i >= Math.ceil((itemsTotal / itemsPerPage - 1)))) {
+      pageNumbers.push(<Pagination.Item onClick={() => {
+        changePage(i);
+        setPageIndex(i);
+        }} key={i}>
         {i}
       </Pagination.Item>);
     } else if (i === 4) {
-      pageNumbers.push(<Pagination.Item onClick={() => changePage(i)} key={i}>
+      pageNumbers.push(<Pagination.Item onClick={() => {
+        changePage(i);
+        setPageIndex(i);
+        }} key={i}>
         {i}
       </Pagination.Item>);
       pageNumbers.push(<Pagination.Ellipsis />,);
@@ -21,13 +28,25 @@ const PageSelector = ({ itemsPerPage, itemsTotal, changePage }) => {
 
   return (
     <>
-      <div>
+      <div className="pagination">
         <Pagination>
-          <Pagination.First />
-          <Pagination.Prev />
+          <Pagination.First onClick={() => {
+            changePage(1);
+            setPageIndex(1);
+            }} />
+          <Pagination.Prev onClick={() => {
+            changePage(pageIndex - 1);
+            setPageIndex(pageIndex - 1);
+            }} />
           {pageNumbers}
-          <Pagination.Next />
-          <Pagination.Last />
+          <Pagination.Next onClick={() => {
+            changePage(pageIndex + 1);
+            setPageIndex(pageIndex + 1);
+            }}/>
+          <Pagination.Last onClick={() => {
+            changePage(pageNumbers.length -1);
+            setPageIndex(pageNumbers.length -1);
+            }}/>
         </Pagination>
       </div>
     </>
