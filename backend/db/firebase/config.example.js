9930ca56461d1303f8803c33c3c0aa95681671f2
Ximing Chen <steven.xm824@gmail.com>
2022-01-03 15:16:27 -0800

    added config example for firebase

---
+++ backend/db/firebase/config.example.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: " auth domain",
+  projectId: "project id ",
+  storageBucket: "storage domain",
+  messagingSenderId: " app id ",
+  appId: " app id "
+};
